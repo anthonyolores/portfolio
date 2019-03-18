3f20f6ccbe0f1ec9842c8476c43463e505dcc155
Anthony Olores <anthony@ABL.local>
2019-03-18 18:24:33 +1300

    Updates

---
+++ precache-manifest.c079da57f6427d192d19731fb77b3030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "83173c53f1a9731bbf89",
+    "url": "/portfolio/static/js/main.1a63befd.chunk.js"
+  },
+  {
+    "revision": "4bd3fc68cf9e8e59da46",
+    "url": "/portfolio/static/js/2.52e305b8.chunk.js"
+  },
+  {
+    "revision": "83173c53f1a9731bbf89",
+    "url": "/portfolio/static/css/main.052d5543.chunk.css"
+  },
+  {
+    "revision": "a7719c945656c2231b163716660e7b0d",
+    "url": "/portfolio/index.html"
+  }
+];
